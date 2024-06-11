6c523eb7ea3ea421b529c07b1152fedeb39768ac
Bjørn Jansen <tolaklaw7@gmail.com>
2024-06-11 13:07:41 +0200

    Gjort koden litt mer robust

--- Send-TelnetBytes.ps1
+++ Send-TelnetBytes.ps1
@@ -1,30 +1,30 @@
 # Default parameter bør fungere. 
-# Men det kan være at du må bruke -discard parameter dersom dingsen du forsøker å koble opp til ikke 
-# sender svar tilbake.
 
 param (
 	$ipaddr='192.168.1.110', 
 	$port=23, 
 	$waittime=500, 
-	[byte[]]$message = @([byte]13,10),
-	[switch]$discard)
+	[byte[]]$message = @([byte]13,10)
+)
 
 try {
 	$c = New-Object System.Net.Sockets.TcpClient($ipaddr, $port)
 	$str = $c.GetStream()
-	#$msg = [System.BitConverter]::GetBytes($message)
 	$msg = $message
 	foreach ($byt in $msg) {
-		Write-Host "Writing byte [$byt]"
-
+		Write-Host "Writing [$byt]"
 		if ($byt -eq 0) {continue; }
 		$str.Write($byt, 0, 1 )
-		$buf = New-Object System.Byte[] 4096
-		if ($discard -eq $false){
-			$count = $str.Read($buf, 0, 4096)
-			[System.Text.Encoding]::ASCII.GetString($buf, 0, $count)
-		}
 		Start-Sleep -Milliseconds $waittime
+		if ($str.DataAvailable){
+			$buf = New-Object System.Byte[] 4096						
+			$count = $str.Read($buf, 0, 4096)
+			for ($i=0; $i -le $count-1 ; $i++)
+			{
+				$value = $buf[$i]
+				Write-Host "Received [$value]"
+			}
+		}
 	}
 	$str.Close()
 	$c.Close()
--- Send-TelnetCommand.ps1
+++ Send-TelnetCommand.ps1
@@ -1,13 +1,10 @@
 # Default parameter bør fungere. 
-# Men det kan være at du må bruke -discard parameter dersom dingsen du forsøker å koble opp til ikke 
-# sender svar tilbake.
 
 param (
 	$ipaddr='192.168.1.110', 
 	$port=23, 
 	$waittime=500, 
-	$message = "c0i4m3n3e", 
-	[switch]$discard
+	$message = "c0i4m3n3e", 	
 )
 
 $msg = [System.Text.Encoding]::ASCII.GetBytes($message)
@@ -16,14 +13,21 @@ try {
 	$str = $c.GetStream()
 
 	foreach ($byt in $msg) {
-		Write-Host "Writing byte [$byt]"
+		Write-Host "Writing [$byt]"
 		$str.Write($byt, 0, 1 )
-		$buf = New-Object System.Byte[] 4096
-		if ($discard -eq $false){
-			$count = $str.Read($buf, 0, 4096)
-			[System.Text.Encoding]::ASCII.GetString($buf, 0, $count)
-		}
+
 		Start-Sleep -Milliseconds $waittime
+
+		if ($str.DataAvailable)
+		{
+			$buf = New-Object System.Byte[] 4096						
+			$count = $str.Read($buf, 0, 4096)
+			for ($i=0; $i -le $count-1 ; $i++)
+			{
+				$value = $buf[$i]
+				Write-Host "Received [$value]"
+			}
+		}
 	}
 	$str.Close()
 	$c.Close()
